d7bc00616df1cf5546eefbc8ab90766800203339
racagogi <racagogi@gmail.com>
2023-06-07 02:36:14 +0900

    treesitter config

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,64 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = "all",
+            sync_install = true,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<M-s>",
+                    node_incremental = "<M-a>",
+                    node_decremental = "<M-x>",
+                },
+            },
+            indent = {
+                enable = true
+            },
+            playground = {
+                enable = true,
+                disable = {},
+                updatetime = 25,
+                persist_queries = false,
+                keybindings = {
+                    toggle_query_editor = 'o',
+                    toggle_hl_groups = 'i',
+                    toggle_injected_languages = 't',
+                    toggle_anonymous_nodes = 'a',
+                    toggle_language_display = 'I',
+                    focus_language = 'f',
+                    unfocus_language = 'F',
+                    update = 'R',
+                    goto_node = '<cr>',
+                    show_help = '?',
+                },
+            },
+            rainbow = {
+                enable = true,
+            },
+            autotag = {
+                enable = true,
+            },
+            matchup = {
+                enable = true,
+            },
+        }
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+        vim.opt.foldenable = false
+        vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end
+}
---
+++ lua/plugins/vim-matchup.lua
@@ -0,0 +1,2 @@
+return {"andymass/vim-matchup"
+}
