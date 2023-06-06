a7a46630f5bbc82515b8e7398c8c7518f44cb7a1
racagogi <racagogi@gmail.com>
2023-06-06 23:26:31 +0900

    baisc options

---
+++ lua/settings/options.lua
@@ -0,0 +1,26 @@
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.autoread = true
+
+vim.opt.autoindent = true
+vim.opt.cindent = false
+vim.opt.expandtab=true
+vim.opt.tabstop=4
+vim.opt.shiftwidth=4
+vim.opt.smarttab=true
+vim.opt.hlsearch=true
+vim.opt.incsearch=true
+vim.opt.ignorecase=true
+vim.opt.smartcase=true
+vim.opt.cursorline=true
+vim.opt.linebreak=true
+vim.opt.list=true
+vim.opt.listchars="tab:>>="
+vim.opt.showbreak="<>"
+vim.opt.wrap=true
+vim.opt.number=true
+vim.opt.showmatch=true
+vim.opt.clipboard="unnamedplus"
+vim.opt.termguicolors=true
+vim.opt.shell="zsh"
+vim.opt.mouse=""
