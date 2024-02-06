87c6ec1c1fe949bf424b4b16f5d7aa80062f73b1
racagogi <racagogi@gmail.com>
2024-02-06 22:33:40 +0900

    fix: gruvbox italic error

--- lua/plugins/util/gruvbox.lua
+++ lua/plugins/util/gruvbox.lua
@@ -2,29 +2,12 @@ return {
     "ellisonleao/gruvbox.nvim",
     config = function()
         require("gruvbox").setup({
-            undercurl = true,
-            underline = true,
-            bold = true,
-            italic = {
-                strings = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            strikethrough = true,
-            invert_selection = false,
-            invert_signs = false,
-            invert_tabline = false,
-            invert_intend_guides = false,
-            inverse = true,
             contrast = "soft",
-            palette_overrides = {},
-            overrides = {},
-            dim_inactive = false,
-            transparent_mode = false,
+            overrides = {
+                ["@markup.italic"] = {italic = true}
+            },
         })
         vim.o.background = "light"
         vim.cmd("colorscheme gruvbox")
     end,
-    priority = 1000
 }
--- lua/settings/autocmds.lua
+++ lua/settings/autocmds.lua
@@ -1,18 +1,39 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*.rkt",
     callback = function()
-        vim.filetype="raket"
+        vim.filetype = "raket"
     end
 })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*.prolog",
     callback = function()
-        vim.filetype="prolog"
+        vim.filetype = "prolog"
     end
 })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*.idiris2",
     callback = function()
-        vim.filetype="idiris2"
+        vim.filetype = "idiris2"
     end
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+    pattern = "*",
+    callback = function()
+        if vim.api.nvim_get_mode().mode ~= "i" then
+            vim.opt.relativenumber = true
+            vim.opt.concealcursor = "n"
+            vim.opt.conceallevel = 3
+            vim.cmd "redraw"
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+    pattern = "*",
+    callback = function()
+        vim.opt.relativenumber = false
+        vim.opt.conceallevel = 0
+        vim.cmd "redraw"
+    end,
 })
